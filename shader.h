9f58e53af154ede2d2aeb0a747f3bb302dae0d9f
Felix Gordillo Yucra <102375834+felix-gy@users.noreply.github.com>
2025-10-31 11:51:31 -0500

    Add setFloat method to Shader class
    Added a method to set a float uniform in the Shader class.

--- shader.h
+++ shader.h
@@ -39,6 +39,10 @@ public:
     void setVec3Array(const std::string &name, int count, const Vec3 *values) const {
         glUniform3fv(glGetUniformLocation(ID, name.c_str()), count, (const GLfloat*)values);
     }
+	
+	void setFloat(const std::string &name, float value) const { 
+		glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
+	}
 
 private:
     void checkCompileErrors(unsigned int shader, std::string type) {
