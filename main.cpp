a50e894858f17ceac0ca4e991a8772948831af15
Felix Gordillo Yucra <102375834+felix-gy@users.noreply.github.com>
2025-10-31 11:38:12 -0500

    Enhance Rubik's Cube rendering with borders
    Updated the Rubik's Cube rendering to include border drawing and improved shader logic.

--- main.cpp
+++ main.cpp
@@ -50,10 +50,24 @@ const char* fragmentShaderSource = R"glsl(
 
     flat in int v_FaceID;
     uniform vec3 u_faceColors[6]; // [R, L, U, D, F, B]
+	uniform float u_isBorder;
 
     void main() {
-        vec3 objectColor = u_faceColors[v_FaceID];
-        FragColor = vec4(objectColor, 1.0);
+		if (u_isBorder > 0.5) {
+            // PASE 2: Estamos dibujando el borde, forzar a negro.
+            FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+			
+        } else {
+            // PASE 1: Estamos dibujando el relleno, usar el color de la cara.
+            vec3 objectColor = u_faceColors[v_FaceID];
+            
+            // Si el fondo es negro (caras internas), píntalo de un gris oscuro.
+            if (objectColor == vec3(0.0, 0.0, 0.0)) {
+                 FragColor = vec4(0.05, 0.05, 0.05, 1.0); // Plástico base
+            } else {
+                 FragColor = vec4(objectColor, 1.0);
+			}
+		}
     }
 )glsl";
 
@@ -126,7 +140,8 @@ public:
     ~RubiksCube() {
         glDeleteVertexArrays(1, &m_VAO);
         glDeleteBuffers(1, &m_VBO);
-		glDeleteBuffers(1, &m_EBO);
+		glDeleteBuffers(1, &m_EBO_relleno);
+		glDeleteBuffers(1, &m_EBO_bordes);
     }
 
 	
@@ -152,25 +167,49 @@ public:
             {{s, s,-s}, 5}, {{s,-s,-s}, 5}, {{-s,-s,-s}, 5}
         };
 		
-		const unsigned int indices[36] = {
+		const unsigned int INDICES_RELLENO[36] = {
 			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 
 			18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35
+		};
+		
+		const unsigned int BORDER_INDICES[24] = {
+
+		// Cara Frontal
+		24, 25,  // Inferior
+		25, 26,  // Derecha (v2)
+		26, 28,  // Superior (v4)
+		28, 24,  // Izquierda (v1)
+		// 2. Contorno de la Cara Trasera (Z-) - Usando los índices 30-35
+		30, 31, // Inferior (de (-s, -s, -s) a (s, -s, -s))
+		31, 32, // Derecha (de (s, -s, -s) a (s, s, -s))
+		32, 34, // Superior (de (s, s, -s) a (-s, s, -s))
+		34, 30, // Izquierda (de (-s, s, -s) a (-s, -s, -s))
+		
+		24, 30, // Inferior-Izquierda
+		25, 34, // Inferior-Derecha
+		26, 32, // Superior-Derecha
+		28, 31  // Superior-Izquierda
+
+
 		};
 		
 		
 
         glGenVertexArrays(1, &m_VAO);
         glGenBuffers(1, &m_VBO);
-		glGenBuffers(1, &m_EBO);
-		
+		glGenBuffers(1, &m_EBO_relleno);
+		glGenBuffers(1, &m_EBO_bordes);
         glBindVertexArray(m_VAO);
         // VBO
 		glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
         glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
-		// EBO
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-		
+		// EBO INDICES_RELLENO
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO_relleno);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(INDICES_RELLENO), INDICES_RELLENO, GL_STATIC_DRAW);
+		// EBO BORDER_INDICES
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO_bordes); 
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(BORDER_INDICES), BORDER_INDICES, GL_STATIC_DRAW);
+		//
         GLsizei stride = sizeof(Vertex);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)offsetof(Vertex, pos));
         glEnableVertexAttribArray(0);
@@ -196,18 +235,29 @@ public:
             faceColors[3] = getVec3FromColor(cubie.getFaceColor(Face::DOWN));
             faceColors[4] = getVec3FromColor(cubie.getFaceColor(Face::FRONT));
             faceColors[5] = getVec3FromColor(cubie.getFaceColor(Face::BACK));
-            shader.setVec3Array("u_faceColors", 6, faceColors);
-			// 
-            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-
+			shader.setVec3Array("u_faceColors", 6, faceColors);
+			
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO_relleno); 
+			shader.setFloat("u_isBorder", 0.0f);
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+			 
+			glLineWidth(3.0f);
+			shader.setFloat("u_isBorder", 1.0f);
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO_bordes); 
+			glDrawElements(GL_LINES, 24, GL_UNSIGNED_INT, 0);
+            //glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
         }
         glBindVertexArray(0);
     }
 
 private:
     std::array<Cubie, 27> m_cubies;
-    GLuint m_VAO, m_VBO, m_EBO;
-    const float m_spacing = 1.05f;
+    GLuint m_VAO, m_VBO;
+	GLuint m_EBO_relleno; // EBO para 36 índices (triángulos)
+	GLuint m_EBO_bordes;
+    const float m_spacing = 1.0f;
 
     int getIndex(int x, int y, int z) const { return x + y * 3 + z * 9; }
 
